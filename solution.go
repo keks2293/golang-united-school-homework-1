5d8b581d5ade407fb4c2f2dd53232d1f51cbe4c5
keks2293 <rusik2293@gmail.com>
2022-02-11 23:54:50 +0300

    1

--- solution.go
+++ solution.go
@@ -1,5 +1,14 @@
 package solution
 
-func GetMessage() string {
-	return ""
+import (
+	"github.com/kyokomi/emoji/"
+)
+
+func main() {
+	//	fmt.Println("Hello World Emoji!")
+
+	emoji.Println("Hello :world_map:!")
+
+	//	pizzaMessage := emoji.Sprint("I like a :pizza: and :sushi:!!")
+	//	fmt.Println(pizzaMessage)
 }
