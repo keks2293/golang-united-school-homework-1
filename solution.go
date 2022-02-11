f6757eaafb76bf08fb57a24d197e0faf29d1f93d
keks2293 <rusik2293@gmail.com>
2022-02-12 00:34:38 +0300

    3

--- solution.go
+++ solution.go
@@ -5,10 +5,5 @@ import (
 )
 
 func main() {
-	//	fmt.Println("Hello World Emoji!")
-
 	emoji.Println("Hello :world_map:!")
-
-	//	pizzaMessage := emoji.Sprint("I like a :pizza: and :sushi:!!")
-	//	fmt.Println(pizzaMessage)
 }
